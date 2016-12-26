2efdf7259be257c62e481c00c56893b2314aff1e
Ohgyun Ahn <ohgyun@gmail.com>
2016-12-26 16:40:51 +0900

    Initial commit

---
+++ run_notebook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+container_name=ds_notebook
+image_repository=jupyter/datascience-notebook
+host_port=9999
+
+if [[ -n $(docker ps -q -f name=${container_name}) ]]; then
+    echo "Container is already running"
+elif [[ -n $(docker ps -aq -f name=${container_name}) ]]; then
+    echo "Restart container"
+    docker restart $container_name
+else
+    echo "Create and run new container"
+    docker run -d \
+        -p $host_port:8888 \
+        --name $container_name \
+        -v $(pwd)/notebooks:/notebooks \
+        -v $(pwd)/data:/data \
+        $image_repository
+fi
+
+sleep 1
+open http://localhost:$host_port
